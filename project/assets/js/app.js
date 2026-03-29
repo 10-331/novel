f912a849a53ba4ec65dcc789923b642fee9e4eb3
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-03-30 00:52:39 +0900

    Update app.js

--- project/assets/js/app.js
+++ project/assets/js/app.js
@@ -306,34 +306,6 @@ el.stage.addEventListener("click", () => {
   renderLine();
 });
 
-
-const el = {
-  viewport: document.getElementById("gameViewport"),
-  stage: document.getElementById("stage"),
-  bg: document.getElementById("bg"),
-  nameMain: document.getElementById("nameMain"),
-  nameSub: document.getElementById("nameSub"),
-  text: document.getElementById("text"),
-  next: document.getElementById("nextIndicator"),
-  overlay: document.getElementById("orientationOverlay"),
-
-  menuBtn: document.getElementById("menuBtn"),
-  menuPanel: document.getElementById("menuPanel"),
-  backBtn: document.getElementById("backBtn"),
-  skipBtn: document.getElementById("skipBtn"),
-
-  endChoiceOverlay: document.getElementById("endChoiceOverlay"),
-  continueBtn: document.getElementById("continueBtn"),
-  finishBtn: document.getElementById("finishBtn"),
-
-  chars: [
-    document.getElementById("char1"),
-    document.getElementById("char2"),
-    document.getElementById("char3"),
-    document.getElementById("char4")
-  ]
-};
-
 const menuBtn = document.getElementById("menuBtn");
 const menuPanel = document.getElementById("menuPanel");
 
