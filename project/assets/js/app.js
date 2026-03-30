cffcc3cdf0c6886066515d17af1d10999bd3cb4f
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-03-31 00:52:12 +0900

    Update app.js

--- project/assets/js/app.js
+++ project/assets/js/app.js
@@ -345,9 +345,9 @@ function getPositionLeftValue(position) {
   switch (position) {
     case "single": return 50;
     case "far-left": return 14;
-    case "left": return 38;
+    case "left": return 36;
     case "center": return 50;
-    case "right": return 62;
+    case "right": return 64;
     case "far-right": return 86;
     default: return 50;
   }
