8e3ab0ed33098e5dd00d74285af0201d5386b4c5
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-03-30 22:11:48 +0900

    Update motionPresets.js

--- project/assets/js/motionPresets.js
+++ project/assets/js/motionPresets.js
@@ -1,56 +1,67 @@
 export const MOTION_PRESETS = {
-  enterTwo(params) {
+  enterTwo(params = {}) {
     return [
       {
         type: "enter",
         targets: [
-          { id: params.a, to: "left" },
-          { id: params.b, to: "right" }
+          { id: params.a, to: params.aTo || "left", x: params.aX || 0, y: params.aY || 0 },
+          { id: params.b, to: params.bTo || "right", x: params.bX || 0, y: params.bY || 0 }
         ],
-        duration: 1000
+        duration: params.duration || 1000
       }
     ];
   },
 
-  addRightAfterShift(params) {
+  addRightAfterShift(params = {}) {
     return [
       {
         type: "move",
         targets: [
-          { id: params.existing, to: "left" }
+          {
+            id: params.existing,
+            to: params.existingTo || "left",
+            x: params.existingX || 0,
+            y: params.existingY || 0
+          }
         ],
-        duration: 500
+        duration: params.moveDuration || 500
       },
       {
         type: "wait",
-        duration: 180
+        duration: params.waitDuration || 180
       },
       {
         type: "enter",
         targets: [
-          { id: params.newChar, to: "right" }
+          {
+            id: params.newChar,
+            to: params.newTo || "right",
+            x: params.newX || 0,
+            y: params.newY || 0
+          }
         ],
-        duration: 1000
+        duration: params.enterDuration || 1000
       }
     ];
   },
 
-  exitTogether(params) {
+  exitTogether(params = {}) {
     return [
       {
         type: "exit",
-        targets: params.ids,
-        duration: 700
+        targets: params.ids || [],
+        duration: params.duration || 700
       }
     ];
   },
 
-  panicMove(params) {
+  panicMove(params = {}) {
+    const target = params.target;
     return [
-      { type: "move", targets: [{ id: params.target, to: "center" }], duration: 180 },
-      { type: "move", targets: [{ id: params.target, to: "right" }], duration: 180 },
-      { type: "move", targets: [{ id: params.target, to: "left" }], duration: 180 },
-      { type: "move", targets: [{ id: params.target, to: "right" }], duration: 180 }
+      { type: "move", targets: [{ id: target, to: "center" }], duration: params.stepDuration || 180 },
+      { type: "move", targets: [{ id: target, to: "right" }], duration: params.stepDuration || 180 },
+      { type: "move", targets: [{ id: target, to: "left" }], duration: params.stepDuration || 180 },
+      { type: "move", targets: [{ id: target, to: "right" }], duration: params.stepDuration || 180 }
     ];
   }
 };
