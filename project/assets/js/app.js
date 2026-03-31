9231a624871a52d2346d8058a9372f6f6edf6550
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-04-01 01:05:05 +0900

    Update app.js

--- project/assets/js/app.js
+++ project/assets/js/app.js
@@ -1,14 +1,3 @@
-/* ===== ベース ===== */
-html, body{
-  margin:0;
-  padding:0;
-  width:100%;
-  height:100%;
-  overflow:hidden;
-  font-family:"Hiragino Mincho ProN","Yu Mincho","Noto Serif JP",serif;
-  background:#000;
-}
-
 /* ===== ビューポート ===== */
 .app{
   width:100vw;
