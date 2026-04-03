7a2abcb150ac762b222eb7d222d377ffa22ad029
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-04-04 02:05:24 +0900

    Update app.js

--- project/assets/js/app.js
+++ project/assets/js/app.js
@@ -16,8 +16,8 @@ const EPISODE_FILES = [
 ];
 
 const EPISODE_TITLES = [
-  { num: "第一話", sub: "タイトル" },
-  { num: "第二話", sub: "タイトル" }
+  { num: "壊生カメリア　1話", sub: "TRACK.1" },
+  { num: "壊生カメリア　2話", sub: "TRACK.2" }
 ];
 
 const el = {
