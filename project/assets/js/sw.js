3aa04c7355243fe33fae0d09becb371f585ca7f8
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-04-10 12:40:25 +0900

    Create sw.js

---
+++ project/assets/js/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "novel-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./assets/css/style.css",
+  "./assets/js/app.js"
+];
+
+// インストール時
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// リクエスト時
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
